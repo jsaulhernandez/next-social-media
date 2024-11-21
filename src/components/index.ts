c55ffb8c9ba01ff7965696a7e88f66554193ffbc
Sony <jsaulhernandez98@gmail.com>
2024-11-21 15:57:49 -0600

    tag: adding new component to home page

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import Navbar from "@/components/Navbar";
+import MobileMenu from "@/components/MobileMenu";
+import LeftMenu from "@/components/LeftMenu";
+import RightMenu from "@/components/RightMenu";
+import AddPost from "@/components/AddPost";
+import Stories from "@/components/Stories";
+import Feed from "@/components/Feed";
+
+export { Navbar, MobileMenu, LeftMenu, RightMenu, AddPost, Stories, Feed };
