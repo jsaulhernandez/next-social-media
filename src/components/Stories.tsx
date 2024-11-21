76d13a001a48da7bbcc075d616e6bc52c059a4aa
Sony <jsaulhernandez98@gmail.com>
2024-11-21 17:51:09 -0600

    tag: adding ui to stories

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+
 const Stories = () => {
-  return <div>Stories</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="/noAvatar.png"
+            alt="user image"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Sony</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stories;
