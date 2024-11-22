8c3215dc91be8e4f2b4f405f9759047173786a64
Sony <jsaulhernandez98@gmail.com>
2024-11-22 13:28:43 -0600

    tag: finish home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import { AddPost, Feed, LeftMenu, RightMenu, Stories } from "@/components";
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="flex gap-6 py-6">
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu />
+      </div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <Stories />
+          <AddPost />
+          <Feed />
+        </div>
+      </div>
+      <div className="hidden lg:block w-[30%] xl:w-[20%]">
+        <RightMenu userId="1" />
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
