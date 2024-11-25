f928ef6b6afc2a5a644ebbc7b14d8ef5f171976f
Sony <jsaulhernandez98@gmail.com>
2024-11-25 13:47:29 -0600

    tag: rename param

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 // components
 import { Feed, LeftMenu, RightMenu } from "@/components";
 
-const Profile = () => {
+// actions
+import { getUserInfoByUserName, isUserBlocker } from "@/lib/actions";
+
+const Profile = async ({ params }: { params: { username: string } }) => {
+  const userInfo = await getUserInfoByUserName(params.username);
+  if (!userInfo) return notFound();
+
+  const isBlocked = await isUserBlocker(userInfo.id);
+  if (isBlocked) return notFound();
+
   return (
     <div className="flex gap-6 py-6">
       <div className="hidden xl:block w-[20%]">
@@ -15,13 +25,13 @@ const Profile = () => {
           <div className="flex flex-col items-center justify-center">
             <div className="w-full h-64 relative">
               <Image
-                src="https://images.pexels.com/photos/411994/pexels-photo-411994.jpeg?auto=compress&cs=tinysrgb&w=600"
+                src={userInfo.cover ?? "/noCover.png"}
                 alt="cover image"
                 fill
                 className="rounded-md object-cover"
               />
               <Image
-                src="https://images.pexels.com/photos/5256093/pexels-photo-5256093.jpeg?auto=compress&cs=tinysrgb&w=600"
+                src={userInfo.avatar || "/noAvatar.png"}
                 alt="profile image"
                 width={128}
                 height={125}
@@ -29,21 +39,31 @@ const Profile = () => {
               />
             </div>
 
-            <h1 className="mt-20 mb-4 text-2xl font-medium">Sony Dev</h1>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">
+              {userInfo.name && userInfo.surname
+                ? userInfo.name + " " + userInfo.surname
+                : userInfo.username}
+            </h1>
 
             <div className="flex items-center justify-center gap-12 mb-4">
               <div className="flex flex-col items-center">
-                <span className="font-medium">123</span>
+                <span className="font-medium">
+                  {userInfo.posts.length ?? 0}
+                </span>
                 <span className="text-sm">Posts</span>
               </div>
 
               <div className="flex flex-col items-center">
-                <span className="font-medium">1.2K</span>
+                <span className="font-medium">
+                  {userInfo.followers.length ?? 0}K
+                </span>
                 <span className="text-sm">Followers</span>
               </div>
 
               <div className="flex flex-col items-center">
-                <span className="font-medium">1.3K</span>
+                <span className="font-medium">
+                  {userInfo.followings.length ?? 0}K
+                </span>
                 <span className="text-sm">Following</span>
               </div>
             </div>
@@ -54,7 +74,7 @@ const Profile = () => {
       </div>
 
       <div className="hidden lg:block w-[30%] xl:w-[20%]">
-        <RightMenu userId="1" />
+        <RightMenu user={userInfo} />
       </div>
     </div>
   );
