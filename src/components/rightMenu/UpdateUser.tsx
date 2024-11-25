f56cece304aea3f61dff5a6d7ae03c37120814a6
Sony <jsaulhernandez98@gmail.com>
2024-11-25 14:16:28 -0600

    fix: sending default value to cover image

--- src/components/rightMenu/UpdateUser.tsx
+++ src/components/rightMenu/UpdateUser.tsx
@@ -41,7 +41,10 @@ const UpdateUser = ({ user }: { user: IUser }) => {
         <div className="absolute w-screen h-screen top-0 left-0 bg-black bg-opacity-65 flex items-center justify-center z-50 ">
           <form
             action={(formData) =>
-              formAction({ formData, cover: cover?.secure_url || "" })
+              formAction({
+                formData,
+                cover: cover?.secure_url || "/noCover.png",
+              })
             }
             className="p-12 bg-white rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3 relative"
           >
